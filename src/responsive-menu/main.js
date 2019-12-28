18c098bbf7c9ea3cb5d061024a5623ce4d9b38c2
Ieva Sinkeviciute <ieva1919@gmail.com>
2019-12-28 19:49:07 +0200

    accordion with javascript and black menu for mobile

--- src/responsive-menu/main.js
+++ src/responsive-menu/main.js
@@ -4,3 +4,8 @@ import { Menu } from './Menu'
 window.addEventListener('DOMContentLoaded', () => {
     new Menu(document.getElementById('main-menu'))
 })
+
+const burger = document.getElementsByClassName('tlsft-menu--burger')[0];
+burger.addEventListener('click', function() {
+ 
+})

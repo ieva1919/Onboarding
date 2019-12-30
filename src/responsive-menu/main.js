2ad8c8b3218d0d2ae985c619655f9d59b8a9d2af
Ieva Sinkeviciute <ieva1919@gmail.com>
2019-12-30 22:59:49 +0200

    some changes in menu for mobile

--- src/responsive-menu/main.js
+++ src/responsive-menu/main.js
@@ -14,3 +14,13 @@ const close = document.getElementsByClassName('tlsft-menu--close')[0];
 close.addEventListener('click', function() {
     document.getElementsByClassName('tlsft-menuformobile-black')[0].classList.remove('open')
 })
+
+const group1 = document.getElementsByClassName('tlsft-menu__group1')[0];
+group1.addEventListener('click', function() {
+    document.getElementsByClassName('tlsft-menuformobile-form')[0].classList.add('open')
+})
+
+const menuform = document.getElementsByClassName('tlsft-menuform--close')[0];
+menuform.addEventListener('click', function() {
+    document.getElementsByClassName('tlsft-menuformobile-form')[0].classList.remove('open')
+})

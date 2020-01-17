77e8300c57e6443fcb2ef3174296df93d532e469
Ieva Sinkeviciute <ieva1919@gmail.com>
2020-01-17 22:40:22 +0200

    change of menu

--- src/responsive-menu/main.js
+++ src/responsive-menu/main.js
@@ -5,33 +5,27 @@ window.addEventListener('DOMContentLoaded', () => {
     new Menu(document.getElementById('main-menu'))
 })
 
-// const burgerButton = document.getElementsByClassName('tlsft-menu__burger')[0]
-// const burgerMenu = document.getElementsByClassName('tlsft-menuformobile-black')[0]
-// const closeButton = document.getElementsByClassName('tlsft-menu__close')[0]
+const burgerButton = document.getElementsByClassName('tlsft-menu__burger')[0]
+const menuContent = document.getElementsByClassName('tlsft-menu-content')[0]
+const closeButton = document.getElementsByClassName('tlsft-menu__close')[0]
 
-// burgerButton.addEventListener('click', function() {
-//     burgerMenu.classList.add('open')
-// })
+burgerButton.addEventListener('click', function() {
+    menuContent.classList.add('tlsft-menu__black')
+})
 
-// closeButton.addEventListener('click', function() {
-//     burgerMenu.classList.remove('open')
-// })
+closeButton.addEventListener('click', function() {
+    menuContent.classList.remove('tlsft-menu__black')
+})
 
-// const loginButton = document.getElementsByClassName('tlsft-menu__group1')[0]
-// const loginMenu = document.getElementsByClassName('tlsft-menuformobile-form')[0]
-// const closeMenuForm = document.getElementsByClassName('tlsft-menuform--close')[0]
 
-// loginButton.addEventListener('click', function() {
-//     loginMenu.classList.add('open')
-// })
+const loginButton = document.getElementsByClassName('menu-login-button')[0]
+const loginForm = document.getElementsByClassName('tlsft-menuformobile-form')[0]
+const closeMenuForm = document.getElementsByClassName('tlsft-menuform--close')[0]
 
-// closeMenuForm.addEventListener('click', function() {
-//     loginMenu.classList.remove('open')
-// })
+loginButton.addEventListener('click', function() {
+    loginForm.classList.add('open')
+})
 
-// const loginButtonMenu = document.getElementsByClassName('menu-login-button')[0]
-// const loginMenu1 = document.getElementsByClassName('tlsft-menuformobile-form')[0]
-
-// loginButtonMenu.addEventListener('click', function() {
-//     loginMenu1.classList.add('open')
-// })
+closeMenuForm.addEventListener('click', function() {
+    loginForm.classList.remove('open')
+})
